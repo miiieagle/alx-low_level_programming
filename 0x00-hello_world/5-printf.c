14cd1aa00b83df64d5c5c9d9aae3c35ba9cdee29
miieagle <miieagle06@gmail.com>
2022-09-08 15:39:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(Mystr);
+	return (0);
+}
