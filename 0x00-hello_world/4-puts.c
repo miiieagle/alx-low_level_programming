d67db6fc3e50092f9e1299b057a35b29cf55912c
miieagle <miieagle06@gmail.com>
2022-09-08 13:53:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include &lt;stdio.h&gt;
+
+
+
+/**
+ *
+ * * main - print the string in the put function
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints &quot;Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
