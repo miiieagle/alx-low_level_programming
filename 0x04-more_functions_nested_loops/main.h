12ac5a8003edfe59626297e0618ba01df7d1a043
miieagle <miieagle06@gmail.com>
2022-09-15 09:11:51 -0700

    prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
