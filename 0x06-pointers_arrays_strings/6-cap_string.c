21690e5bb87f60458a8374c30cfd7317eb6e2ac8
miieagle <miieagle06@gmail.com>
2022-09-22 01:26:31 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,30 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- * @str: string to change
- * Return: string changed
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *str)
-{
-	int i = 0, j = 0, tmp = 0;
-	char sep[] = {'\n', ' ', '\t', ',', ';', '"',
-		'(', ')', '{', '}', '?', '!', '.'};
 
-	while (str[i] != '\0')
+char *cap_string(char *s)
+{
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		tmp = 0;
-		for (j = 0; sep[j] != '\0' && i != 0; j++)
+		for (i = 0; i < 13; i++)
 		{
-			if (str[i - 1] == sep[j])
+			if (*(s + count) == sep_words[i])
 			{
-				tmp = 1;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
-		if (i == 0 || tmp == 1)
-		{
-			if (str[i] > 96 && str[i] < 123)
-				str[i] -= 32;
-		}
-		i++;
+		count++;
 	}
-
-	return (str);
+	return (s);
 }
