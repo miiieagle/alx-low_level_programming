9ec8a204903c4725947e3290bdbbd7ebb9e64139
miieagle <miieagle06@gmail.com>
2022-09-28 11:22:34 -0700

    Just made a 0-puts_recursion.c file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print a string recursively
+ * @s: string to print
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
