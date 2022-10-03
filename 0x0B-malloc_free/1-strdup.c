276702fe0f47fd05956a9e02c0f3b76697bf519d
miieagle <miieagle06@gmail.com>
2022-10-03 10:41:15 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - return length of a string
+ * @s: char type
+ * Return:  length of string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+	return (a);
+}
+
+/**
+ * *_strdup - function to return pointer to string
+ * @str: pointer to string array input
+ * Return: pointer to string created
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int size;
+	int x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = _strlen(str) + 1;
+	ptr = malloc(size * sizeof(char));
+
+	x = 0;
+	while (x < size)
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		ptr[x] = str[x];
+		x++;
+	}
+	return (ptr);
+}
